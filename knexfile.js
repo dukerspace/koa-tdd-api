bde01da4bfa6320ed938d571daea637659b2edc7
Valentino Gagliardi <valentino@valentinog.com>
2017-08-30 11:40:54 +0200

    Change from development to test

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,10 @@ const path = require("path");
 const BASE_PATH = path.join(__dirname, "server", "db");
 
 module.exports = {
-  development: {
+  test: {
     client: "sqlite3",
     connection: {
-      filename: "./dev.sqlite3"
+      filename: "./test.sqlite3"
     },
     migrations: {
       directory: path.join(BASE_PATH, "migrations")
