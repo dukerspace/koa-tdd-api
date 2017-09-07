7858c139ffd8b5f3afcea9694d67e0ee88d92ae8
Valentino Gagliardi <valentino@valentinog.com>
2017-09-07 10:04:37 +0200

    Replace Mocha with Jest

--- test/articles.routes.test.js
+++ test/articles.routes.test.js
@@ -4,10 +4,13 @@ const server = require("../server/index");
 const knex = require("knex")(config);
 const PATH = "/api/v1/articles";
 
-const chai = require("chai");
-const should = chai.should();
-const chaiHttp = require("chai-http");
-chai.use(chaiHttp);
+const request = require("supertest");
+
+const sampleObj = {
+  title: "A test article",
+  body: "The test article's body",
+  tags: "test, nodejs"
+};
 
 describe("routes: articles", () => {
   beforeEach(() => {
@@ -22,76 +25,56 @@ describe("routes: articles", () => {
   });
 
   afterEach(() => {
-    return knex.migrate.rollback();
+    return knex.migrate.rollback().then(() => {
+      server.close();
+    });
   });
 
   describe(`GET ${PATH}`, () => {
-    it("should return all the resources", done => {
-      chai
-        .request(server)
-        .get(`${PATH}`)
-        .end((err, res) => {
-          should.not.exist(err);
-          res.status.should.eql(200);
-          res.type.should.eql("application/json");
-          res.body.data.length.should.eql(2);
-          res.body.data[0].should.include.keys("id", "title", "body", "tags");
-          done();
-        });
+    test("should return all the resources", async () => {
+      const res = await request(server).get(`${PATH}`);
+      expect(res.status).toEqual(200);
+      expect(res.type).toEqual("application/json");
+      expect(res.body.data.length).toEqual(2);
+      expect(res.body.data[0]).toHaveProperty("id");
+      expect(res.body.data[0]).toHaveProperty("title");
+      expect(res.body.data[0]).toHaveProperty("body");
+      expect(res.body.data[0]).toHaveProperty("tags");
     });
   });
 
   describe(`GET ${PATH}/:id`, () => {
-    it("should return a single resource", done => {
-      chai
-        .request(server)
-        .get(`${PATH}/1`)
-        .end((err, res) => {
-          should.not.exist(err);
-          res.status.should.eql(200);
-          res.type.should.eql("application/json");
-          res.body.data.length.should.eql(1);
-          res.body.data[0].should.include.keys("id", "title", "body", "tags");
-          done();
-        });
+    test("should return a single resource", async () => {
+      const res = await request(server).get(`${PATH}/1`);
+      expect(res.status).toEqual(200);
+      expect(res.type).toEqual("application/json");
+      expect(res.body.data.length).toEqual(1);
+      expect(res.body.data[0]).toHaveProperty("id");
+      expect(res.body.data[0]).toHaveProperty("title");
+      expect(res.body.data[0]).toHaveProperty("body");
+      expect(res.body.data[0]).toHaveProperty("tags");
     });
-    it("should return an error when the requested resource does not exists", done => {
-      chai
-        .request(server)
-        .get(`${PATH}/9999`)
-        .end((err, res) => {
-          should.exist(err);
-          res.status.should.eql(404);
-          res.type.should.eql("application/json");
-          res.body.error.should.eql("The requested resource does not exists");
-          done();
-        });
+    test("should return an error when the requested resource does not exists", async () => {
+      const res = await request(server).get(`${PATH}/9999`);
+      expect(res.status).toEqual(404);
+      expect(res.type).toEqual("application/json");
+      expect(res.body.error).toEqual("The requested resource does not exists");
     });
   });
 
   describe(`POST ${PATH}`, () => {
-    it("should return the newly added resource identifier alongside a Location header", done => {
-      chai
-        .request(server)
+    test("should return the newly added resource identifier alongside a Location header", async () => {
+      const res = await request(server)
         .post(`${PATH}`)
-        .send({
-          title: "A test article",
-          body: "The test article's body",
-          tags: "test, nodejs"
-        })
-        .end((err, res) => {
-          should.not.exist(err);
-          res.status.should.eql(201);
-          res.should.have.header("Location");
-          res.type.should.eql("application/json");
-          res.body.data.length.should.eql(1);
-          res.body.data[0].should.be.a("number");
-          done();
-        });
+        .send(sampleObj);
+      expect(res.status).toEqual(201);
+      expect(res.header).toHaveProperty("location");
+      expect(res.type).toEqual("application/json");
+      expect(res.body.data.length).toEqual(1);
+      expect(typeof res.body.data[0]).toEqual("number");
     });
-    it("should return an error when the resource already exists", done => {
-      chai
-        .request(server)
+    test("should return an error when the resource already exists", async () => {
+      const res = await request(server)
         .post(`${PATH}`)
         .send({
           title:
@@ -99,14 +82,10 @@ describe("routes: articles", () => {
           body:
             "An Introduction to Building Test Driven RESTful APIs with Koa ... body",
           tags: "koa, tdd, nodejs"
-        })
-        .end((err, res) => {
-          should.exist(err);
-          res.status.should.eql(409);
-          res.type.should.eql("application/json");
-          res.body.error.should.eql("The resource already exists");
-          done();
         });
+      expect(res.status).toEqual(409);
+      expect(res.type).toEqual("application/json");
+      expect(res.body.error).toEqual("The resource already exists");
     });
   });
 });
